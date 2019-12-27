40d574fd81db9cff8d914f7d3b2ceed16324386a
Suchan Lee <suchanl@palantir.com>
2019-12-27 11:05:43 -0800

    fix

--- src/renderer/components/Counter.tsx
+++ src/renderer/components/Counter.tsx
@@ -3,23 +3,23 @@ import * as React from "react";
 require("./Counter.scss");
 
 export interface Props {
-    value: number;
-    incrementValue: () => any;
-    decrementValue: () => any;
+  value: number;
+  incrementValue: () => any;
+  decrementValue: () => any;
 }
 
 const Counter: React.FunctionComponent<Props> = ({ value, incrementValue, decrementValue }) => (
-    <div className="counter">
-        <p id="counter-value">Current value: {value}</p>
-        <p>
-            <button id="increment" onClick={incrementValue}>
-                Increment
-            </button>
-            <button id="decrement" onClick={decrementValue}>
-                Decrement
-            </button>
-        </p>
-    </div>
+  <div className="counter">
+    <p id="counter-value">Current value: {value}</p>
+    <p>
+      <button id="increment" onClick={incrementValue}>
+        Increment
+      </button>
+      <button id="decrement" onClick={decrementValue}>
+        Decrement
+      </button>
+    </p>
+  </div>
 );
 
 export default Counter;
