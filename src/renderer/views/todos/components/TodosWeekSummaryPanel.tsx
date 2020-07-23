cef56c8ef6ec34d3f01e161839b5976044d3c7ee
suchanlee <lee.suchan@gmail.com>
2020-07-23 12:17:50 -0700

    fixes to summary panel

--- src/renderer/views/todos/components/TodosWeekSummaryPanel.tsx
+++ src/renderer/views/todos/components/TodosWeekSummaryPanel.tsx
@@ -1,9 +1,11 @@
-import React, { memo, useCallback } from "react";
+import { Switch } from "@blueprintjs/core";
+import React, { memo, useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { PanelContainer } from "../../../shared-components/PanelContainer";
 import { TodosActions } from "../redux/todosActions";
 import { selectSummaryTodosWeek, selectTodosSummaryDate } from "../redux/todosSelectors";
 import { TodoStatus } from "../redux/todosTypes";
+import { linkifyText } from "../utils/linkifyText";
 import {
   createTodayTodoDate,
   isTodoDatesEqual,
@@ -26,6 +28,7 @@ const DAY_OF_WEEK = [
 export const TodosWeekSummaryPanel = memo(() => {
   const date = useSelector(selectTodosSummaryDate);
   const week = useSelector(selectSummaryTodosWeek);
+  const [showInProgress, setShowInProgress] = useState(true);
   const dispatch = useDispatch();
   const handleClose = useCallback(() => {
     dispatch(TodosActions.setSummary(undefined));
@@ -42,30 +45,45 @@ export const TodosWeekSummaryPanel = memo(() => {
     >
       {date != null && (
         <div className="todos-week-summary-panel">
+          <div>
+            <Switch
+              checked={showInProgress}
+              onChange={() => setShowInProgress(!showInProgress)}
+              label="Show In Progress Todos"
+            />
+          </div>
           {week.map(day => {
             const dayOfWeek = DAY_OF_WEEK[todoDateToDate(day.date).getDay()];
             const isToday = isTodoDatesEqual(day.date, todayDate);
+            const finishedTodos = day.todos.filter(todo => todo.status === TodoStatus.FINISHED);
+            const inProgressTodos = day.todos.filter(
+              todo => todo.status === TodoStatus.IN_PROGRESS
+            );
             return (
               <React.Fragment key={dayOfWeek}>
                 <h2>
                   {dayOfWeek} {isToday ? "(TODAY)" : ""}
                 </h2>
-                <h4 className="todos-week-summary-panel-finished">FINISHED</h4>
-                <ul>
-                  {day.todos
-                    .filter(todo => todo.status === TodoStatus.FINISHED)
-                    .map(task => (
-                      <li key={task.id}>{task.value}</li>
-                    ))}
-                </ul>
-                <h4 className="todos-week-summary-panel-in-progress">IN PROGRESS</h4>
-                <ul>
-                  {day.todos
-                    .filter(todo => todo.status === TodoStatus.IN_PROGRESS)
-                    .map(task => (
-                      <li key={task.id}>{task.value}</li>
-                    ))}
-                </ul>
+                {finishedTodos.length > 0 && (
+                  <React.Fragment>
+                    <h4 className="todos-week-summary-panel-finished">FINISHED</h4>
+                    <ul>
+                      {finishedTodos.map(task => (
+                        <li key={task.id}>{linkifyText(task.value)}</li>
+                      ))}
+                    </ul>
+                  </React.Fragment>
+                )}
+                {showInProgress && inProgressTodos.length > 0 && (
+                  <React.Fragment>
+                    <h4 className="todos-week-summary-panel-in-progress">FINISHED</h4>
+                    <ul>
+                      {inProgressTodos.map(task => (
+                        <li key={task.id}>{linkifyText(task.value)}</li>
+                      ))}
+                    </ul>
+                  </React.Fragment>
+                )}
               </React.Fragment>
             );
           })}
